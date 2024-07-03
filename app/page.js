70c49d87e522a359969d185a72d45040010ed671
r2hu1 <newasram86@gmail.com>
2024-07-03 22:28:17 +0530

    ftr

--- app/page.js
+++ app/page.js
@@ -1,11 +1,23 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 export default function Page() {
   return (
     <main>
       <Hero />
       <Projects />
+      <section className="px-6 md:px-20 lg:px-32 py-5">
+        <div>
+          <p className="text-sm text-center text-gray-400">Coded with ❤️ by <a className="underline hover:text-gray-300 transition" href="https://github.com/r2hu1">r2hu1</a></p>
+        </div>
+      </section>
     </main>
   );
 }

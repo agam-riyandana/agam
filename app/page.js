cd04343ad731b8bf39e1ef92f0106ab50509391f
r2hu1 <newasram86@gmail.com>
2024-07-05 11:15:43 +0530

    orgn

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 
@@ -6,11 +7,7 @@ export default function Page() {
     <main>
       <Hero />
       <Projects />
-      <section className="px-6 md:px-20 lg:px-32 py-5">
-        <div>
-          <p className="text-sm text-center text-gray-400">Coded with ❤️ by <a className="underline hover:text-gray-300 transition" href="https://github.com/r2hu1">r2hu1</a></p>
-        </div>
-      </section>
+      <Footer/>
     </main>
   );
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <section className="px-6 md:px-20 lg:px-32 py-5">
+            <div>
+                <p className="text-sm text-center text-gray-400">Coded with ❤️ by <a className="underline hover:text-gray-300 transition" href="https://github.com/r2hu1">r2hu1</a></p>
+            </div>
+        </section>
+    )
+}

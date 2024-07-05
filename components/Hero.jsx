d4b08672abd9da40a66f71caf6c7f4a77dce89c8
r2hu1 <newasram86@gmail.com>
2024-07-05 11:41:02 +0530

    responsive resume viewer

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -36,18 +36,68 @@ export default function Hero() {
                 <p className="text-base text-gray-400 -mt-1 mb-3">A full stack developer with a passion for creating modern and intuitive web applications.</p>
                 <div className="flex gap-3">
                     {!isBreakpoint ? (
-                        <Drawer>
-                            <DrawerTrigger asChild>
-                                <Button variant="shine">GetIn Touch</Button>
-                            </DrawerTrigger>
-                            <DrawerContent>
-                                <DrawerHeader>
-                                    <DrawerTitle>GetIn Touch</DrawerTitle>
-                                    <DrawerDescription>Fill in the form to get in touch with me.</DrawerDescription>
-                                </DrawerHeader>
-                                <DrawerFooter>
+                        <>
+                            <Drawer>
+                                <DrawerTrigger asChild>
+                                    <Button variant="shine">GetIn Touch</Button>
+                                </DrawerTrigger>
+                                <DrawerContent>
+                                    <DrawerHeader>
+                                        <DrawerTitle>GetIn Touch</DrawerTitle>
+                                        <DrawerDescription>Fill in the form to get in touch with me.</DrawerDescription>
+                                    </DrawerHeader>
+                                    <DrawerFooter>
+                                        <form>
+                                            <div className="grid gap-4">
+                                                <Label htmlFor="name" className="-mb-2">Name</Label>
+                                                <Input id="name" placeholder="Name" type="text" required />
+                                                <Label htmlFor="email" className="-mb-2">Email</Label>
+                                                <Input id="email" placeholder="Email" type="email" required />
+                                                <Label htmlFor="message" className="-mb-2">Message</Label>
+                                                <Textarea id="message" placeholder="Message" className="min-h-[100px]" required />
+                                                <Button type="submit" className="mt-1" variant="shine">Submit</Button>
+                                                <div className="flex gap-3 items-center">
+                                                    <span className="text-sm text-gray-500">or</span>
+                                                    <div className="flex items-center gap-2">
+                                                        <Link href="https://instagram.com/r.rah_ul">
+                                                            <span className="cursor-pointer hover:bg-primary transition hover:text-primary-foreground bg-secondary/50 p-2 text-xs flex gap-1 items-center w-fit rounded-md px-3"><Instagram className="h-3 w-3" />Instagram</span>
+                                                        </Link>
+                                                        <Link href="mailto:hi@rahul.eu.org">
+                                                            <span className="cursor-pointer hover:bg-primary transition hover:text-primary-foreground bg-secondary/50 p-2 text-xs flex gap-1 items-center w-fit rounded-md px-3">hi@rahul.eu.org</span>
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </DrawerFooter>
+                                </DrawerContent>
+                            </Drawer>
+                            <Drawer>
+                                <DrawerTrigger asChild>
+                                    <Button variant="outline" className="flex items-center gap-1">Resume <IoOpenOutline className="w-4 h-4" /></Button>
+                                </DrawerTrigger>
+                                <DrawerContent>
+                                    <DrawerFooter>
+                                        <iframe src="/resume.pdf" className="h-[500px] rounded-md"></iframe>
+                                    </DrawerFooter>
+                                </DrawerContent>
+                            </Drawer>
+                        </>
+                    ) : (
+                        <>
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <Button variant="shine">GetIn Touch</Button>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>Get in touch</DialogTitle>
+                                        <DialogDescription>
+                                            <p>Fill in the form to get in touch with me.</p>
+                                        </DialogDescription>
+                                    </DialogHeader>
                                     <form>
-                                        <div className="grid gap-4">
+                                        <div className="grid gap-4 py-2">
                                             <Label htmlFor="name" className="-mb-2">Name</Label>
                                             <Input id="name" placeholder="Name" type="text" required />
                                             <Label htmlFor="email" className="-mb-2">Email</Label>
@@ -68,49 +118,18 @@ export default function Hero() {
                                             </div>
                                         </div>
                                     </form>
-                                </DrawerFooter>
-                            </DrawerContent>
-                        </Drawer>
-                    ) : (
-
-                        <Dialog>
-                            <DialogTrigger asChild>
-                                <Button variant="shine">GetIn Touch</Button>
-                            </DialogTrigger>
-                            <DialogContent>
-                                <DialogHeader>
-                                    <DialogTitle>Get in touch</DialogTitle>
-                                    <DialogDescription>
-                                        <p>Fill in the form to get in touch with me.</p>
-                                    </DialogDescription>
-                                </DialogHeader>
-                                <form>
-                                    <div className="grid gap-4 py-2">
-                                        <Label htmlFor="name" className="-mb-2">Name</Label>
-                                        <Input id="name" placeholder="Name" type="text" required />
-                                        <Label htmlFor="email" className="-mb-2">Email</Label>
-                                        <Input id="email" placeholder="Email" type="email" required />
-                                        <Label htmlFor="message" className="-mb-2">Message</Label>
-                                        <Textarea id="message" placeholder="Message" className="min-h-[100px]" required />
-                                        <Button type="submit" className="mt-1" variant="shine">Submit</Button>
-                                        <div className="flex gap-3 items-center">
-                                            <span className="text-sm text-gray-500">or</span>
-                                            <div className="flex items-center gap-2">
-                                                <Link href="https://instagram.com/r.rah_ul">
-                                                    <span className="cursor-pointer hover:bg-primary transition hover:text-primary-foreground bg-secondary/50 p-2 text-xs flex gap-1 items-center w-fit rounded-md px-3"><Instagram className="h-3 w-3" />Instagram</span>
-                                                </Link>
-                                                <Link href="mailto:hi@rahul.eu.org">
-                                                    <span className="cursor-pointer hover:bg-primary transition hover:text-primary-foreground bg-secondary/50 p-2 text-xs flex gap-1 items-center w-fit rounded-md px-3">hi@rahul.eu.org</span>
-                                                </Link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </DialogContent>
-                        </Dialog>
+                                </DialogContent>
+                            </Dialog>
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <Button variant="outline" className="flex items-center gap-1">Resume <IoOpenOutline className="w-4 h-4" /></Button>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <iframe src="/resume.pdf" className="h-[500px] mt-3 w-full rounded-md"></iframe>
+                                </DialogContent>
+                            </Dialog>
+                        </>
                     )}
-
-                    <Button variant="outline" className="flex items-center gap-1">Resume <IoOpenOutline className="w-4 h-4" /></Button>
                 </div>
             </div>
         </section>

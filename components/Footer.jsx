e3492923693b69cab9a28399d2014ff192adbe46
Rahul Rajput <newasram86@gmail.com>
2024-07-07 14:47:12 +0000

    fixed txt

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <section className="px-6 md:px-20 lg:px-32 py-5 mt-10 text-center border-t">
             <div>
-                <p className="text-sm text-gray-300">&copy;2022 <Link href="/" className="text-sm text-gray-400 underline">r2hu1</Link> all rights reserved.</p>
+                <p className="text-sm dark:text-gray-400 text-gray-600">&copy;2022 <Link href="/" className="text-sm text-gray-400 underline">r2hu1</Link> all rights reserved.</p>
             </div>
         </section>
     )

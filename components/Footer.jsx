aca384dd9ca42fead4d1a2d471474d6abea024de
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-07-21 15:29:44 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <section className="px-6 md:px-20 lg:px-32 py-5 mt-10 text-center border-t">
             <div>
-                <p className="text-sm dark:text-gray-400 text-gray-600">&copy;{new Date().getFullYear()}<Link href="/" className="text-sm text-gray-400 underline">Agam Riyandana</Link> All Rights Reserved.</p>
+                <p className="text-sm dark:text-gray-400 text-gray-600">&copy; {new Date().getFullYear()} <Link href="/" className="text-sm text-gray-400 underline">Agam Riyandana</Link> All Rights Reserved.</p>
             </div>
         </section>
     )

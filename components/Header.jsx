e884e613a59147db05cbd18ca73f1f50c0d892a6
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-07-21 15:08:52 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,14 +8,14 @@ export default function Header() {
         <header className="px-6 md:px-20 lg:px-32 py-5 flex items-center justify-between w-full">
             <div className="flex items-center gap-4">
                 <Avatar>
-                    <AvatarImage src="https://raw.githubusercontent.com/agam-riyandana/portofolio-baru/master/src/assets/Me.jpg?token=GHSAT0AAAAAACISHINBGX2AT4STIBIUJRFQZU4X7BQ" />
+                    <AvatarImage src="https://github.com/agam-riyandana/portofolio-baru/blob/6cf6689a3643a1cf953aa82511ef20c9ca3099e1/src/assets/Me.jpg" />
                     <AvatarFallback><Loader2 className="w-4 h-4 animate-spin" /></AvatarFallback>
                 </Avatar>
                 <div className="flex flex-col">
                     <h1 className="text-lg font-medium">Agam Riyandana</h1>
                     <div className="flex items-center gap-1">
                         <div className="h-1.5 w-1.5 animate-pulse rounded-full bg-green-500"></div>
-                        <p className="text-xs text-gray-400 -mt-0.5">Digital Creator</p>
+                        <p className="text-xs text-gray-400 -mt-0.5">Online</p>
                     </div>
                 </div>
             </div>

74239c6cba3edd5e94b27fa6dbacaee8cde9adde
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-07-27 16:08:37 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import { Sponsors } from '@/components/Sponsors';
 
 export default function Page() {
   return (
     <main>
       <Hero />
       <Projects />
+      <Sponsors />
     </main>
   );
 }

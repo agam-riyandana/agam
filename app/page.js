b9c963081e63e6aa037c3929ba1dc3bbb1ef9d2d
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-07-27 17:02:05 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-import { Sponsors } from '@/components/Sponsors';
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <main>
       <Hero />
       <Projects />
+      <div className="flex items-center justify-center mb-10">
+        <Button variant="outline" asChild>
+          <Link href="/">View All</Link>
+        </Button>
+      </div>
     </main>
   );
 }

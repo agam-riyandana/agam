7c300a030cde185a19959ca1ecb4f15c1b5d2e35
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-07-28 07:58:28 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,6 @@ import Header from "@/components/Header";
 import { Toaster } from "@/components/ui/sonner";
 import Footer from "@/components/Footer";
 import Banner from "@/components/Banner";
-import { Analytics } from '@vercel/analytics/react';
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -33,7 +32,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={poppins.className}>
         <Banner />
-        <Analytics />
         <div vaul-drawer-wrapper="" className="antialiased">
           <ThemeProvider
             attribute="class"

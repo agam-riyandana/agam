c98697bc3f03717ed5fc4f773ea4957a135bb5b2
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-07-29 22:05:31 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,9 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  openGraph: {
   title: "Agam Riyandana | Portofolio",
   generator: 'Next.js',
+  manifest: 'https://agamriyandana.my.id/manifest.json',
   applicationName: 'Next.js',
   referrer: 'origin-when-cross-origin',
   keywords: "agam, agamriyandana, my.id, domain Indonesia, Jember, pandi, portofolio, domain nusantara, digital, website, internasional, asia, eropa",

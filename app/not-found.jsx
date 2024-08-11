ba6e57a94a4c0ab0c6c7ce14f8de54477c785f1c
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-11 13:02:09 +0700

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,17 +2,14 @@ import { Button } from "@/components/ui/button";
 import { AlertTriangle } from "lucide-react";
 import Link from "next/link";
 
-export default async function NotFound() {
-  const headersList = headers()
-  const domain = headersList.get('host')
-  const data = await getSiteData(domain)
-  return (
-    <div>
-      <h2>Not Found: {data.name}</h2>
-      <p>Could not find requested resource</p>
-      <p>
-        View <Link href="/">all posts</Link>
-      </p>
-    </div>
-  )
+export default function Page() {
+    return (
+        <div className="z-10 bg-background h-full w-full absolute top-0 left-0 right-0 flex items-center justify-center">
+            <div className="grid gap-2 text-center max-w-xs px-6 place-items-center">
+                <AlertTriangle className="h-5 w-5" />
+                <p className="text-muted-foreground text-sm">The page you are trying to access does not exists.</p>
+                <Button variant="outline" asChild><Link href="/">Back to Homepage</Link></Button>
+            </div>
+        </div>
+    )
 }

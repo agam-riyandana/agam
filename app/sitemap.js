910ec2bb8e90946aaef544961d442fbed0536dd5
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-11 12:44:02 +0700

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://agamriyandana.my.id',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ]
+}
